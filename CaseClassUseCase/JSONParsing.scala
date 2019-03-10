b6ff54bdbf3405eb5e514da473480974c6a69356
Sankar Biswas <sankarb475@gmail.com>
2019-03-10 19:09:42 +0530

    Update JSONParsing.scala

--- CaseClassUseCase/JSONParsing.scala
+++ CaseClassUseCase/JSONParsing.scala
@@ -1,1 +1,32 @@
-//in-progress
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import scala.io.Source
+
+object JSONParsing extends App {
+
+  implicit val formats : DefaultFormats.type = DefaultFormats // Brings in default date formats etc.
+
+  case class BookDetails(bookId: String, bookName: String, authorName: String, authorCountry: String)
+
+  for (line <- Source.fromFile("/Users/sankar.biswas/Desktop/jsonFile.json").getLines) {
+    val bookDetails = jsonStrToMap(line)
+    println(bookDetails.bookName)
+  }
+
+  def jsonStrToMap(jsonStr: String): BookDetails = {
+    parse(jsonStr).camelizeKeys.extract[BookDetails]
+  }
+}
+
+
+
+//content of jsonFile
+{"book_id":"1","book_name":"Scala","author_name":"Edward","author_country":"Poland"}
+{"book_id":"1","book_name":"Scala","author_name":"Edward","author_country":"Poland"}
+{"book_id":"1","book_name":"Scala","author_name":"Edward","author_country":"Poland"}
+{"book_id":"1","book_name":"Scala","author_name":"Edward","author_country":"Poland"}
+
+//so here you can see case class can give schema to your input dataset, I have had to analyse many uniform complex dataset, and 
+//the best way to handle those data is to use case class and give custom schema. Using custom schema you can easily fetch and 
+//traverse through your data.
+
