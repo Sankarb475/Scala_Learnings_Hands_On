49845c1b8c0099bef1afed7ccaa3a0a5e07c43f7
Sankar Biswas <sankarb475@gmail.com>
2019-07-23 12:35:41 +0530

    Update ScalaFunctions.scala

--- ScalaFunctions.scala
+++ ScalaFunctions.scala
@@ -72,7 +72,30 @@ scala> rdd2.collect.foreach(println)
 (10,CompactBuffer(1))
 (11,CompactBuffer(1))
 
+//while running spark-shell if you want to add some external jar so that you can import some functionalities, follow this ::
 
+./spark-shell --jars /Users/sankar.biswas/Desktop/spark-cassandra-connector_2.11-2.0.10.jar, /Users/dummy.jar2
+
+//Or you can add following configuration in you spark-defaults.conf but remember to remove template from end of spark-defaults
+spark.driver.extraClassPath  pathOfJarsWithCommaSeprated
+
+
+//Higher Order Function => Function which takes a function as input and/or returns a function as output
+
+scala> def dumdum : (String => String) = _.toUpperCase
+dumdum: String => String
+
+scala> dumdum
+res26: String => String = <function1>
+
+scala> val a = "My name is Anthony"
+a: String = My name is Anthony
+
+scala> val b = a.toUpperCase
+b: String = MY NAME IS ANTHONY
+
+scala> val c = dumdum(a)
+c: String = MY NAME IS ANTHONY
 
 
 
