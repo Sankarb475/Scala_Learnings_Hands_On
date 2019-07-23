0f635da5640ad4560488410adcb8685255aa7b2d
Sankar Biswas <sankarb475@gmail.com>
2019-07-23 14:46:26 +0530

    Update MoreScala.scala

--- MoreScala.scala
+++ MoreScala.scala
@@ -1,26 +1,62 @@
-//while running spark-shell if you want to add some external jar so that you can import some functionalities, follow this ::
+The name Scala stands for "scalable language" is a statically typed language.
 
-./spark-shell --jars /Users/sankar.biswas/Desktop/spark-cassandra-connector_2.11-2.0.10.jar, /Users/dummy.jar2
+You can apply Scala to a wide range of programming tasks, from writing small scripts to building large systems.
 
-//Or you can add following configuration in you spark-defaults.conf but remember to remove template from end of spark-defaults
-spark.driver.extraClassPath  pathOfJarsWithCommaSeprated
+Scala is good with small scripting as well. A script is just a sequence of statements in a file that will be executed sequentially.
+
+Write the scala statements into a file hello.scala and run using 
+scala hello.scala
+=======================================================================================================================================
+Difference between Statically typed and dynamically typed languages.
+
+A language is statically typed if the type of a variable is known at compile time. For some languages this means that you as the 
+programmer must specify what type each variable is (e.g.: Java, C, C++); other languages offer some form of type inference, 
+the capability of the type system to deduce the type of a variable 
+(e.g.: OCaml, Haskell, Scala, Kotlin)
+The main advantage here is that all kinds of checking can be done by the compiler, and therefore a lot of trivial bugs are caught at a 
+very early stage.
+
+Scala language is statically typed even when you do not mention the variable type since it can deduce the type of the varibale at compile 
+time.
+
+Dynamically typed languages doesnt need you to define the data type explicitely and these gets deduced at run-time. Most scripting 
+languages are dynamically typed as there is no compiler to do static type-checking anyway. Although you can define the data type explicitely
+but do not require it.
+=======================================================================================================================================
+Why Scala ?
+==>
+1) Scala is compatible
+Scala can get well mixed with Java, as a matter of fact Scala behind the picture uses a lot of Java libraries including the different 
+data types. Run-time performance is usually on par with Java programs. Scala code can call Java methods, access Java fields, inherit from 
+Java classes, and implement Java interfaces.
+
+2) Scala is concise
+Scala programs tend to be short. It takes half of the line which Java takes for same task. Its functional side helps you implement the
+same logic in small.
+
+3) Scala is high-level
+Scala helps you manage complexity by letting you raise the level of abstraction in the interfaces you design and use
+
+4) Scala is statically typed but it can deduce the data type most of the time.
+==================================================================================================================================Scala=====
+Scala Functions ::
+
+scala> def greet() = print("Hello World")                                                                                                                                       
+greet: ()Unit                                                                                                                                                                   
+                                                                                                                                                                                
+scala> greet                                                                                                                                                                    
+Hello World  
 
 
-//Higher Order Function => Function which takes a function as input and/or returns a function as output
 
-scala> def dumdum : (String => String) = _.toUpperCase
-dumdum: String => String
 
-scala> dumdum
-res26: String => String = <function1>
 
-scala> val a = "My name is Anthony"
-a: String = My name is Anthony
 
-scala> val b = a.toUpperCase
-b: String = MY NAME IS ANTHONY
 
-scala> val c = dumdum(a)
-c: String = MY NAME IS ANTHONY
 
-// I will convert this function into a Spark UDF in sparkLearnings.scala file in this repository only
+
+
+
+
+
+
