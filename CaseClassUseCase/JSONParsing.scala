471b0b85bd4b70b83814a1a333a34e526e8fc67c
Sankar Biswas <sankarb475@gmail.com>
2021-05-28 18:56:38 +0530

    Update JSONParsing.scala

--- CaseClassUseCase/JSONParsing.scala
+++ CaseClassUseCase/JSONParsing.scala
@@ -26,6 +26,12 @@ object JSONParsing extends App {
 {"book_id":"1","book_name":"Scala","author_name":"Edward","author_country":"Poland"}
 {"book_id":"1","book_name":"Scala","author_name":"Edward","author_country":"Poland"}
 
+// scala case classes are normal classes which are immutable in nature and used especially for pattern matching
+
+// It has a pre-defined apply method, which initiates the objects, thus we dont need a new keyword while creatig the object 
+// of a case class.
+
+
 //so here you can see case class can give schema to your input dataset, I have had to analyse many uniform complex dataset, and 
 //the best way to handle those data is to use case class and give custom schema. Using custom schema you can easily fetch and 
 //traverse through your data.
